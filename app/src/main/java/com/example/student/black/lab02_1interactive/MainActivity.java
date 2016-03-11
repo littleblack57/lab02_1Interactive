d602b0bd959d28993b7a2cd00b49292da466ddc4
littleblack57 <littleblack57@hotmail.com>
2016-03-11 12:12:40 +0800

    Add "+" & "-" & "order" Button

--- app/src/main/java/com/example/student/black/lab02_1interactive/MainActivity.java
+++ app/src/main/java/com/example/student/black/lab02_1interactive/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.student.black.lab02_1interactive;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,9 +12,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    int a = 0 ;
     public void submitOrder(View view) {
-        TextView number = (TextView)findViewById(R.id.num);
-        number.setText("1");
+        priceDisplay(a);
+
     }
+
+    public void add(View view){
+
+        int y = a += 1;
+        quandisplay(y);
+
+    }
+
+    public void subtract(View view) {
+        int y = a -= 1 ;
+        if(y <1 ){
+            y = 0;
+            a = 0;
+        }
+        quandisplay(y);
+
+    }
+
+
+
+    private void priceDisplay(int a) {
+        TextView price = (TextView)findViewById(R.id.price);
+        price.setText("NT$ "+String.valueOf(a*10));
+    }
+
+    private void quandisplay(int a) {
+        TextView number = (TextView)findViewById(R.id.num);
+        number.setText(String.valueOf(a));
+
+
+    }
+
+
 }
