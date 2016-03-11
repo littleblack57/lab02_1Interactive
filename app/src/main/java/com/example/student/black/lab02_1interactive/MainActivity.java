71e8783f818c53a700a33a09386d04752839340f
littleblack57 <littleblack57@hotmail.com>
2016-03-11 10:58:57 +0800

    new project

---
+++ app/src/main/java/com/example/student/black/lab02_1interactive/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.student.black.lab02_1interactive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        TextView number = (TextView)findViewById(R.id.num);
+        number.setText("1");
+    }
+}
